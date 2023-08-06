76c1c27d1bc87edb61490bf64392d13882447972
Dylan Dombrowski <dyland601@gmail.com>
2023-08-06 18:03:21 -0400

    07 complete. onto dom manipulation

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (celsius) {
+  const fahrenheit = (celsius - 32) * (5 / 9);
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (fahrenheit) {
+  const celsius = fahrenheit * (9 / 5) + 32;
+  let celsiusRounded = Math.round(celsius * 10) / 10;
+  return celsiusRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
