f1fa9a623bc53fdd063dd44268110627c16b5d24
Dylan Dombrowski <dyland601@gmail.com>
2023-08-06 17:07:39 -0400

    on to leapyears

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = function (string) {
   return string.split("").reverse().join("");
 };
-//study this one. extremely easy and useful once understood.
+
 // Do not edit below this line
 module.exports = reverseString;
