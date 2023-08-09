0c50cb0e74763079fc316ec4b2dc21eceb6e2b31
Dylan Dombrowski <dyland601@gmail.com>
2023-08-08 21:46:46 -0400

    08 calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function (x, y) {
+  let sum = x + y;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  let difference = x - y;
+  return difference;
 };
 
-const sum = function() {
-	
+function sum(...args) {
+  if (args.length === 0) {
+    throw new Error("At least one number or array is required.");
+  }
+
+  if (args.length === 1) {
+    const numOrArray = args[0];
+
+    if (Array.isArray(numOrArray)) {
+      return numOrArray.reduce((acc, val) => acc + val, 0);
+    } else {
+      throw new Error("When providing a single argument, it must be an array.");
+    }
+  }
+
+  return args.reduce((acc, val) => acc + val, 0);
+}
+
+function multiply(...args) {
+  if (args.length === 0) {
+    throw new Error("At least one number or array is required.");
+  }
+
+  if (args.length === 1) {
+    const numOrArray = args[0];
+
+    if (Array.isArray(numOrArray)) {
+      return numOrArray.reduce((acc, val) => acc * val, 1);
+    } else {
+      throw new Error("When providing a single argument, it must be an array.");
+    }
+  }
+
+  return args.reduce((acc, val) => acc * val, 1);
+}
+
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0) return -1;
+  else if (num == 0) return 1;
+  else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +63,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
