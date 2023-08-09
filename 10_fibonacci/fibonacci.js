303f04349accea9a65161ad5893c023ec63142b0
Dylan J Dombrowski <d0domb01@louisville.edu>
2023-08-09 14:23:30 -0400

    10 fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = parseInt(n); // Convert to integer if it's a string
 
+  if (isNaN(n) || n < 0) {
+    return "OOPS";
+  } else if (n <= 1) {
+    return n;
+  }
+
+  let fibPrev = 0;
+  let fibCurrent = 1;
+
+  for (let i = 2; i <= n; i++) {
+    [fibPrev, fibCurrent] = [fibCurrent, fibPrev + fibCurrent];
+  }
+
+  return fibCurrent;
 };
 
 // Do not edit below this line
