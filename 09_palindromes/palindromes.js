190aba13a74b3f5b077e462fa8c71484bd1b029b
Dylan J Dombrowski <d0domb01@louisville.edu>
2023-08-09 14:00:16 -0400

    09 palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let forwardString = word
+    .toLowerCase()
+    .replace(/[^\w\s]|_/g, "")
+    .replace(/\s+/g, "")
+    .split("");
+  let first = forwardString.join();
+  let second = forwardString.reverse().join();
 
+  if (first === second) return true;
+  else return false;
 };
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
